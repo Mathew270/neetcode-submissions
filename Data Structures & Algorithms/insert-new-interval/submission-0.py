910d4016fdc893e177e01fc3270fbfdf039b8724
Mathew Paul <mathewpaul2004@gmail.com>
2026-07-28 15:00:12 +0800

    Add: insert-new-interval - submission-0

---
+++ Data Structures & Algorithms/insert-new-interval/submission-0.py
@@ -0,0 +1,22 @@
+class Solution:
+    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+        res = []
+
+        for i in range(len(intervals)):
+            # if end of curr-interval is less than start of newInterval
+            # we can safely add curr-interval to res
+            if intervals[i][1] < newInterval[0]:
+                res.append(intervals[i])
+
+            # if end of newInterval is less than start of curr-interval
+            # res = newInterval + rest of intervals
+            elif newInterval[1] < intervals[i][0]:
+                return res + [newInterval] +  intervals[i:]
+
+            else: # overlap
+                newInterval = [min(intervals[i][0], newInterval[0]), max(intervals[i][1], newInterval[1])]
+
+        # if we reach here means we newInterval is yet to be added
+        # this means elif part was never executed
+        res.append(newInterval)
+        return res
