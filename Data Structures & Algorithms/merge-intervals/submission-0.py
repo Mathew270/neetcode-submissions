5445ce0dc2397f6f2492276badac93a4cb2fdfe3
Mathew Paul <mathewpaul2004@gmail.com>
2026-07-28 15:34:43 +0800

    Add: merge-intervals - submission-0

---
+++ Data Structures & Algorithms/merge-intervals/submission-0.py
@@ -0,0 +1,23 @@
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        # sort based on starting pos
+        intervals.sort(key=lambda x: x[0])
+        res = []
+        merge = False
+        new = intervals[0]
+
+        for i in range(1, len(intervals)):
+            if new[1] < intervals[i][0]:
+                res.append(new)
+                new = intervals[i]
+                merge = False
+
+            else: # overlap
+                new = [min(new[0], intervals[i][0]), max(new[1], intervals[i][1])]
+                merge = True
+
+        res.append(new)
+        return res
+
+        
+
